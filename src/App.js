f1a2101e2478ed1175fd01e70d0f21e36c82edb2
Brock Cannon <bcannon@lifestyle.local>
2021-11-10 14:09:39 +1100

    push changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZrEjAj2fjGSz2braVVccN68qDyPapt2g",
+  authDomain: "movie-db-b114f.firebaseapp.com",
+  projectId: "movie-db-b114f",
+  storageBucket: "movie-db-b114f.appspot.com",
+  messagingSenderId: "708627248986",
+  appId: "1:708627248986:web:ae2d10035a23626a6d6563",
+  measurementId: "G-779EYNMWJZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default class App extends React.Component {
+	state = {
+		movieList: [],
+	}
 
 function App() {
   return (
